534bda7002e4b2a9745e2bbe67756dcc8665d395
Vladislav Ivanishin <skytex51rus@gmail.com>
2023-03-05 22:01:10 +1000

    добавляет станицы и компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Settings = {
+  CardsCount: 5,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardsCount={Settings.CardsCount}
+    />
   </React.StrictMode>,
 );
