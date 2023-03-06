56920d7b506d8ab528188999a3eb5dc518e67e8d
Vladislav Ivanishin <skytex51rus@gmail.com>
2023-03-06 23:49:02 +1000

    добавляет маршрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,40 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
+// import FavoritesEmptyScreen from '../../pages/favorites-empty-screen/favorites-empty-screen';
+// import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+// import MainEmptyScreen from '../../pages/main-empty-screen/main-empty-screen';
+// import RoomNotLoggedScreen from '../../pages/room-not-logged-screen/room-not-logged-screen';
+import RoomScreen from '../../pages/room-screen/room-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+import { HelmetProvider } from 'react-helmet-async';
 
 type AppScreenProps = {
   cardsCount: number;
 }
 
-function App({ cardsCount }: AppScreenProps): JSX.Element {
+export default function App({ cardsCount }: AppScreenProps): JSX.Element {
   return (
-    <MainScreen cardsCount={cardsCount} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<MainScreen cardsCount={cardsCount} />} />
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <LoginScreen />
+            </PrivateRoute>
+          }
+          />
+          <Route path={AppRoute.Login} element={<LoginScreen />} />
+          <Route path={AppRoute.Room} element={<RoomScreen />} />
+          <Route path={'*'} element={<NotFoundScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
-export default App;
--- project/src/pages/room-screen/room-screen.tsx
+++ project/src/pages/room-screen/room-screen.tsx
@@ -1,3 +1,5 @@
+import Logo from '../../components/logo/logo';
+
 export default function RoomScreen(): JSX.Element {
   return (
     <div className="page">
@@ -5,14 +7,12 @@ export default function RoomScreen(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
+                  <a className="header__nav-link header__nav-link--profile" href="/">
                     <div className="header__avatar-wrapper user__avatar-wrapper">
                     </div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
@@ -20,7 +20,7 @@ export default function RoomScreen(): JSX.Element {
                   </a>
                 </li>
                 <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
+                  <a className="header__nav-link" href="/">
                     <span className="header__signout">Sign out</span>
                   </a>
                 </li>
@@ -232,7 +232,7 @@ export default function RoomScreen(): JSX.Element {
             <div className="near-places__list places__list">
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
+                  <a href="/">
                     <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
                   </a>
                 </div>
@@ -256,7 +256,7 @@ export default function RoomScreen(): JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Wood and stone place</a>
+                    <a href="/">Wood and stone place</a>
                   </h2>
                   <p className="place-card__type">Private room</p>
                 </div>
@@ -264,7 +264,7 @@ export default function RoomScreen(): JSX.Element {
 
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
+                  <a href="/">
                     <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image" />
                   </a>
                 </div>
@@ -288,7 +288,7 @@ export default function RoomScreen(): JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Canal View Prinsengracht</a>
+                    <a href="/">Canal View Prinsengracht</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
@@ -299,7 +299,7 @@ export default function RoomScreen(): JSX.Element {
                   <span>Premium</span>
                 </div>
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
+                  <a href="/">
                     <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image" />
                   </a>
                 </div>
@@ -323,7 +323,7 @@ export default function RoomScreen(): JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Nice, cozy, warm big bed apartment</a>
+                    <a href="/">Nice, cozy, warm big bed apartment</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
