0c5240ba3878b46e60ecc5eea8a6b6c996188fa3
Vladislav Ivanishin <skytex51rus@gmail.com>
2023-04-05 00:03:35 +1000

    fix lint error

--- project/src/components/favorites/favorites.tsx
+++ project/src/components/favorites/favorites.tsx
@@ -5,17 +5,16 @@ import FavoritesList from './favorites-list';
 export default function Favorites(): JSX.Element {
   const offers = useAppSelector((state) => state.offers);
   const favoriteOffer = offers.filter((offer) => offer.isFavorite === true);
-  console.log(favoriteOffer);
-  return (
-    favoriteOffer.length === 0 ? <FavoritesEmptyScreen /> : (
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <FavoritesList offers={favoriteOffer} />
-          </section>
-        </div >
-      </main >
-    )
+  return favoriteOffer.length === 0 ? (
+    <FavoritesEmptyScreen />
+  ) : (
+    <main className="page__main page__main--favorites">
+      <div className="page__favorites-container container">
+        <section className="favorites">
+          <h1 className="favorites__title">Saved listing</h1>
+          <FavoritesList offers={favoriteOffer} />
+        </section>
+      </div>
+    </main>
   );
 }
