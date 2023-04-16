3ed632c9f3c6535dc0f83a3660219632ef1d334d
Vladislav Ivanishin <skytex51rus@gmail.com>
2023-04-16 23:51:27 +1000

    запросы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -3,60 +3,44 @@ import MainScreen from '../../pages/main-screen/main-screen';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import RoomScreen from '../../pages/room-screen/room-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
-import { AppRoute, AuthorizationStatus } from '../../const';
+import { AppRoute } from '../../const';
 import PrivateRoute from '../private-route/private-route';
 import { HelmetProvider } from 'react-helmet-async';
-import MainEmptyScreen from '../../pages/main-empty-screen/main-empty-screen';
 import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
-import FavoritesEmptyScreen from '../../pages/favorites-empty-screen/favorites-empty-screen';
-import { useAppSelector } from '../../hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import HistoryRouter from '../history-route/history-route';
 import { browserHistory } from '../../browser-history';
-import { Rings } from 'react-loader-spinner';
+import {
+  getErrorStatus,
+  getOffersDataLoadingStatus,
+} from '../../store/data-process/selectors';
+import LoadingScreen from '../../pages/loading-screen/loading-screen';
+import ErrorScreen from '../../pages/error-screen/error-screen';
+import { useEffect } from 'react';
+import { checkAuthAction, fetchOffersAction } from '../../store/api-actions';
 
 export default function App(): JSX.Element {
-  const authorizationStatus = useAppSelector(
-    (state) => state.authorizationStatus
-  );
-  const isOffersDataLoading = useAppSelector(
-    (state) => state.isOffersDataLoading
-  );
-  const offers = useAppSelector((state) => state.offers);
+  const isOffersDataLoading = useAppSelector(getOffersDataLoadingStatus);
+  const hasError = useAppSelector(getErrorStatus);
+  const dispatch = useAppDispatch();
 
-  if (
-    authorizationStatus === AuthorizationStatus.Unknown ||
-    isOffersDataLoading
-  ) {
-    return (
-      <Rings
-        height="250"
-        width="250"
-        color="#4481c3"
-        radius="15"
-        wrapperStyle={{}}
-        wrapperClass=""
-        // eslint-disable-next-line react/jsx-boolean-value
-        visible={true}
-        ariaLabel="rings-loading"
-      />
-    );
+  if (isOffersDataLoading) {
+    return <LoadingScreen />;
+  }
+  if (hasError) {
+    return <ErrorScreen />;
   }
 
-  // const offers = useAppSelector((state) => state.offers);
-  // const comments = useAppSelector((state) => state.comments);
   return (
     <HelmetProvider>
       <HistoryRouter history={browserHistory}>
         <Routes>
-          <Route
-            index
-            element={offers.length ? <MainScreen /> : <MainEmptyScreen />}
-          />
+          <Route index element={<MainScreen />} />
           <Route
             path={AppRoute.Favorites}
             element={
               <PrivateRoute>
-                {offers.length ? <FavoritesScreen /> : <FavoritesEmptyScreen />}
+                <FavoritesScreen />
               </PrivateRoute>
             }
           />
---
+++ project/src/store/data-process/data-process.ts
@@ -0,0 +1,70 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { Comments } from '../../types/comments';
+import { Offer, Offers } from '../../types/offers';
+import { NameSpace } from '../../const';
+import {
+  fetchCommentsAction,
+  fetchOffersAction,
+  fetchOffersFavoriteAction,
+  setFavoriteAction,
+} from '../api-actions';
+
+type DataProcess = {
+  offers: Offers;
+  selectedOffer: Offer | null;
+  nearbyOffers: Offers;
+  favorite: Offers;
+  comments: Comments;
+  isOffersDataLoading: boolean;
+  hasError: boolean;
+};
+
+const initialState: DataProcess = {
+  offers: [],
+  selectedOffer: null,
+  nearbyOffers: [],
+  favorite: [],
+  comments: [],
+  isOffersDataLoading: false,
+  hasError: false,
+};
+
+export const dataProcess = createSlice({
+  name: NameSpace.Data,
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchOffersAction.pending, (state) => {
+        state.isOffersDataLoading = true;
+        state.hasError = false;
+      })
+      .addCase(fetchOffersAction.fulfilled, (state, action) => {
+        state.isOffersDataLoading = false;
+        state.offers = action.payload;
+      })
+      .addCase(fetchOffersAction.rejected, (state) => {
+        state.isOffersDataLoading = false;
+        state.hasError = true;
+      })
+      .addCase(fetchOffersFavoriteAction.fulfilled, (state, action) => {
+        state.isOffersDataLoading = false;
+        state.offers = action.payload;
+      })
+      .addCase(fetchOffersFavoriteAction.rejected, (state) => {
+        state.hasError = true;
+      })
+      .addCase(fetchCommentsAction.fulfilled, (state, action) => {
+        state.comments = action.payload;
+      })
+      .addCase(setFavoriteAction.fulfilled, (state, action) => {
+        if (action.payload.isFavorite) {
+          state.favorite.push(action.payload);
+        } else {
+          state.favorite = state.favorite.filter(
+            ({ id }) => id !== action.payload.id
+          );
+        }
+      });
+  },
+});
